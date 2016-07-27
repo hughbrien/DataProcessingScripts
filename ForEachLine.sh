132203463c34b331b82bf39d48c38d21d19863c4
Hugh Brien <hugh@hughbrien.com>
2016-07-27 00:31:14 -0400

    For each line two ways
    For each line two ways

---
+++ ForEachLine.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+filename='./ElephantOneYear.csv'
+#echo Start
+#while read p; do 
+#    echo $p
+#done < $filename
+
+
+filelines=`cat $filename`
+
+for line in $filelines ; do
+#    newLine= "${line//\,/}"
+done
